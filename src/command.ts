45909633097d25a0e5fd2e277e8fd50338e3f2b6
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-19 10:39:47 -0800

    fix: update config

--- src/command.ts
+++ src/command.ts
@@ -1,16 +1,20 @@
 const pjson = require('../package.json')
-import {ConfigOptions, IConfig, IPlugin, isIConfig, PluginConfig} from '@dxcli/config'
+import * as Config from '@dxcli/config'
 import {args} from '@dxcli/parser'
 import {HTTP} from 'http-call'
 
 import deps from './deps'
 import * as flags from './flags'
 
-export type CommandRunFn = <T extends Command>(this: ICommandClass<T>, argv?: string[], config?: IConfig | ConfigOptions) => Promise<T>
+export interface Options {
+  root?: string
+}
+
+export type CommandRunFn = <T extends Command>(this: ICommandClass<T>, argv?: string[], opts?: Options) => Promise<T>
 
 export interface ICommandClass<T extends Command> {
   run: CommandRunFn
-  new (config: IConfig): T
+  new (): T
 }
 
 const parentModule = module.parent && module.parent.parent && module.parent.parent.filename
@@ -26,26 +30,17 @@ export default abstract class Command {
   static flags: flags.Input
   static args: args.IArg[] = []
   static _base = `${pjson.name}@${pjson.version}`
-  static plugin: IPlugin | undefined
+  static plugin: Config.IPlugin | undefined
 
   /**
    * instantiate and run the command
    */
-  static run: CommandRunFn = async function (argv: string[] = process.argv.slice(2), config: IConfig | ConfigOptions = {}) {
-    if (!isIConfig(config)) config = await PluginConfig.create({root: parentModule!, ...config})
-    const cmd = new this(config as any)
-    try {
-      await cmd.init(argv)
-      await cmd.run()
-      await cmd.done()
-    } catch (err) {
-      // throw HelpErr to allow the CLI to do something with it
-      if (err.code === 'EHELP') throw err
-      deps.cli.error(err)
-    }
-    return cmd
+  static run: CommandRunFn = function (argv: string[] = process.argv.slice(2), opts: Options = {}) {
+    const cmd = new this()
+    return cmd._run(argv, opts)
   }
 
+  config: Config.IConfig
   flags: { [name: string]: any } = {}
   argv: string[]
   args: { [name: string]: string } = {}
@@ -67,18 +62,33 @@ export default abstract class Command {
 
   get http(): typeof HTTP { return require('http-call').HTTP }
 
-  constructor(protected config: IConfig) {
-    global['http-call'] = global['http-call'] || {}
-    global['http-call']!.userAgent = config.userAgent
-    this.debug = require('debug')(`cli:command:${this.ctor.id || config.name}`)
-  }
-
   /**
    * actual command run code goes here
    */
   abstract async run(): Promise<void>
 
-  protected async init(argv: string[]) {
+  protected async _run(argv: string[], opts: Options) {
+    try {
+      await this.init(argv, opts)
+      await this.run()
+      await this.done()
+    } catch (err) {
+      // throw HelpErr to allow the CLI to do something with it
+      switch (err.code) {
+        case 'EEXIT': break
+        case 'EHELP': throw err
+        default:
+          deps.cli.error(err)
+      }
+    }
+    return this
+  }
+
+  protected async init(argv: string[], {root}: {root?: string} = {}) {
+    this.config = await Config.read({root: root || parentModule!})
+    global['http-call'] = global['http-call'] || {}
+    global['http-call']!.userAgent = this.config.userAgent
+    this.debug = require('debug')(`cli:command:${this.ctor.id || this.config.name}`)
     this.debug('init version: %s argv: %o', this.ctor._base, argv)
     deps.cli.config.errlog = this.config.errlog
     try {
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import Command from './command'
+import Command, {Options} from './command'
 import * as flags from './flags'
 
 export * from './errors'
 
 export default Command
-export {Command, flags}
+export {
+  Command,
+  Options,
+  flags,
+}
