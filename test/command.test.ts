3e18992ba8c05aeeea0baf4cb51613ae50bd1c55
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-25 02:55:24 -0800

    fix: updated deps

--- test/command.test.ts
+++ test/command.test.ts
@@ -1,8 +1,8 @@
 import * as Config from '@dxcli/config'
-import {describe, expect, it, output} from '@dxcli/dev-test'
 import cli from 'cli-ux'
+import {expect, fancy} from 'fancy-mocha'
 
-import Base, {HelpErr} from '../src'
+import Base from '../src'
 
 class Command extends Base {
   async run() {
@@ -10,40 +10,50 @@ class Command extends Base {
   }
 }
 
-describe.stdout.stderr('run', () => {
-  it('logs to stdout', async () => {
-    await Command.run([])
-    expect(output.stdout).to.equal('foo\n')
-  })
+describe('run', () => {
+  fancy()
+  .stdout()
+  .run(() => Command.run([]))
+  .run(output => expect(output.stdout).to.equal('foo\n'))
+  .end('logs to stdout')
 
-  it('errors out', async () => {
+  fancy()
+  .run(() => {
     const Command: Config.ICommand = class extends Base {
       async run() {
         throw new Error('new x error')
       }
     }
 
-    await expect(Command.run([])).to.eventually.be.rejectedWith(/new x error/)
+    return Command.run([])
   })
+  .catch(/new x error/)
+  .end('errors out')
 
-  it('exits', async () => {
+  fancy()
+  .stdout()
+  .run(() => {
     const Command: Config.ICommand = class extends Base {
       async run() {
         cli.exit(0)
       }
     }
-
-    await expect(Command.run([])).to.eventually.be.rejectedWith(/EEXIT: 0/)
+    return Command.run([])
   })
+  .catch(/EEXIT: 0/)
+  .end('exits with 0')
 
   describe('help error', () => {
     ['-h', '--help', 'help'].forEach(arg => {
-      it(`throws help error when passed "${arg}"`, async () => {
-        await expect(Command.run([arg])).to.eventually.be.rejectedWith(HelpErr)
-      })
+      fancy()
+      .run(() => Command.run([arg]))
+      .catch((err: any) => expect(err.code).to.equal('EHELP'))
+      .end(`throws help error when passed "${arg}"`)
     })
-    it('does not throw help error when passed "foo"', async () => {
-      await expect(Command.run(['foo'])).to.not.eventually.be.rejectedWith(HelpErr)
-    })
+
+    fancy()
+    .run(() => Command.run(['foo']))
+    .catch((err: any) => expect(err.code).not.to.equal('EHELP'))
+    .end('does not throw help error when passed "foo"')
   })
 })
