bbfc5f72c5e304c8a32a21cd2fd3295c8c19a9eb
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-02-03 05:34:52 -0800

    fix: add run function

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as Config from '@anycli/config'
 import Command from './command'
 import * as flags from './flags'
 export {parse} from '@anycli/parser'
-export {Main} from './main'
+export {run, Main} from './main'
 
 export default Command
 export {
--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,16 @@ import {Command} from '.'
 export class Main extends Command {
   static parserOptions = {'--': false, strict: false}
 
-  static run(argv = process.argv.slice(2), opts?: Config.Options) {
-    return super.run(argv, opts || module.parent && module.parent.parent && module.parent.parent.filename || __dirname)
+  static run(argv = process.argv.slice(2), options?: Config.Options) {
+    return super.run(argv, options || module.parent && module.parent.parent && module.parent.parent.filename || __dirname)
   }
 
   async run() {
     const [id, ...argv] = this.argv
     await this.config.runCommand(id, argv)
   }
+}
+
+export function run(argv = process.argv.slice(2), options?: Config.Options) {
+  return Main.run(argv, options)
 }
