6d090aefd78fd13a556e625f8e28b445476912ee
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-02-03 05:25:34 -0800

    fix: fixed parent module path

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
+import * as Config from '@anycli/config'
+
 import {Command} from '.'
 
 export default class Main extends Command {
   static parserOptions = {'--': false, strict: false}
+
+  static run(argv = process.argv.slice(2), opts?: Config.Options) {
+    return super.run(argv, opts || module.parent && module.parent.parent && module.parent.parent.filename || __dirname)
+  }
 
   async run() {
     const [id, ...argv] = this.argv
