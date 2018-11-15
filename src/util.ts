42179f45a462a7915faf3a4ce5ce3c811feb0e3b
Borek Bernard <borekb@gmail.com>
2018-11-15 10:41:30 +0100

    fix: splitArgv algorithm, more tests

--- src/util.ts
+++ src/util.ts
@@ -5,18 +5,28 @@ export function compact<T>(a: (T | undefined)[]): T[] {
 /**
  * Splits argv to command ID and the rest of the array.
  *
- * For space-separated subcommands, the ID is built from multiple items
- * in the source array.
+ * For space-separated subcommands, the ID is built from multiple items in
+ * the source array. For colon-separated subcommands, the argv item is the ID.
  */
 export function splitArgv(argv: string[], commandIDs: string[]) {
   let argvIndex = 0
   let id = ''
   let idCandidate = argv[argvIndex]
 
-  while (commandIDs.some(commandID => commandID.startsWith(idCandidate))) {
-    id = idCandidate
+  while (commandIDs.includes(idCandidate) || !id) {
+    if (commandIDs.includes(idCandidate)) {
+      id = idCandidate
+    }
     argvIndex++
+    if (argvIndex > argv.length) {
+      break
+    }
+
     idCandidate += ` ${argv[argvIndex]}`
+  }
+
+  if (id === '') {
+    throw new Error('Command ID not found')
   }
 
   return {
--- test/util.test.ts
+++ test/util.test.ts
@@ -23,6 +23,36 @@ describe('splitArgv', () => {
       {id: 'user add', argv: ['Peter']}
     )
   })
+
+  it('Similar but not really - topic level', () => {
+    expect(() => splitArgv(['user', 'add', 'Peter'], ['user2'])).to.throw(Error)
+  })
+
+  it('Similar but not really - nested level', () => {
+    expect(() => splitArgv(['user', 'add', 'Peter'], ['user add2'])).to.throw(Error)
+  })
+
+  it('Multiple nesting levels', () => {
+    expectEqual(
+      splitArgv(['user', 'config', 'frontend', 'add', 'key', 'value'], ['user config frontend add']),
+      {id: 'user config frontend add', argv: ['key', 'value']}
+    )
+  })
+
+  it('Multiple nesting levels, ending in the middle', () => {
+    expectEqual(
+      splitArgv(['user', 'config', 'frontend', 'add', 'key', 'value'], ['user config frontend']),
+      {id: 'user config frontend', argv: ['add', 'key', 'value']}
+    )
+  })
+
+  it('Flags', () => {
+    expectEqual(
+      splitArgv(['user', 'add', '--name', 'Peter'], ['user add']),
+      {id: 'user add', argv: ['--name', 'Peter']}
+    )
+  })
+
 })
 
 // Small strongly-typed helper
