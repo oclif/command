a41984f5712aa85812b1d25fcb8a137b90fb6471
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2019-11-18 14:00:46 -0800

    lint

--- src/util.ts
+++ src/util.ts
@@ -1,10 +1,10 @@
 export function compact<T>(a: (T | undefined)[]): T[] {
-  return a.filter((a): a is T => !!a)
+  return a.filter((a): a is T => Boolean(a))
 }
 
 export function uniqBy<T>(arr: T[], fn: (cur: T) => any): T[] {
   return arr.filter((a, i) => {
-    let aVal = fn(a)
+    const aVal = fn(a)
     return !arr.find((b, j) => j > i && fn(b) === aVal)
   })
 }
@@ -19,7 +19,7 @@ export function sortBy<T>(arr: T[], fn: (i: T) => sort.Types | sort.Types[]): T[
 
     if (Array.isArray(a) && Array.isArray(b)) {
       if (a.length === 0 && b.length === 0) return 0
-      let diff = compare(a[0], b[0])
+      const diff = compare(a[0], b[0])
       if (diff !== 0) return diff
       return compare(a.slice(1), b.slice(1))
     }
